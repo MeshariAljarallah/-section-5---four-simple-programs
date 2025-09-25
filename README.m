12f6a19ab42c05034ae8bd7cc9b69e88b6a5ed2f
Meshari Al-Jarallah <meshari.aljaralla@gmail.com>
2025-09-25 11:18:22 +0300

    Create README.m
    🏢 section 5 - four simple programs:
    
    
    2.5.6 LAB Improving the Caesar Cipher
    
    2.5.7 LAB Palindromes
    
    2.5.8 LAB Anagrams
    
    2.5.9 LAB The Digit of Life
    
    2.5.10 LAB Find a Word!
    
    2.5.11 LAB Sudoku

---
+++ README.m
@@ -0,0 +1,14 @@
+🏢 section 5 - four simple programs:
+
+
+2.5.6 LAB Improving the Caesar Cipher
+
+2.5.7 LAB Palindromes
+
+2.5.8 LAB Anagrams
+
+2.5.9 LAB The Digit of Life
+
+2.5.10 LAB Find a Word!
+
+2.5.11 LAB Sudoku
